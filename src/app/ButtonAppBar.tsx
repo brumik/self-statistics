79426824a54a8fb20fbd47fb5c2ebcab739779a1
Levente Berky <levente.berky@gmail.com>
2024-08-13 06:55:59 +0200

    Add link higlight

--- src/app/ButtonAppBar.tsx
+++ src/app/ButtonAppBar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -5,8 +7,12 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import ConnectionIndicatorButton from './ConnectionIndicatorButton';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 
 export default function ButtonAppBar() {
+  const path = usePathname();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -16,10 +22,10 @@ export default function ButtonAppBar() {
           </Typography>
           <ConnectionIndicatorButton />
           <Link href="/" passHref legacyBehavior>
-            <Button color="inherit">Events</Button>
+            <Button color="inherit" variant={path === '/' ? 'contained' : 'text'}>Events</Button>
           </Link>
-          <Link href="/edit-event-types" passHref legacyBehavior>
-            <Button color="inherit">Configuration</Button>
+          <Link href="/config" passHref legacyBehavior>
+            <Button color="inherit" variant={path === '/config' ? 'contained' : 'text'}>Config</Button>
           </Link>
         </Toolbar>
       </AppBar>
